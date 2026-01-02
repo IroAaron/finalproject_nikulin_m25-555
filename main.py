c7f33be028f865b9b677bcef07308c5b9af378fb
Aaron <denniksez@gmail.com>
2026-01-02 12:58:12 +0300

    connect scheduler into main

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 from valutatrade_hub.cli.interface import run_cli
 from valutatrade_hub.logging_config import setup_logging
+from valutatrade_hub.parser_service.scheduler import RatesScheduler
 
+scheduler = RatesScheduler(interval_seconds=120)
 
 def main() -> None:
     print("Первая попытка запустить проект!")
     setup_logging()
+    scheduler.start()
     run_cli()
+    scheduler.stop()
 
 if __name__ == "__main__":
     main()
